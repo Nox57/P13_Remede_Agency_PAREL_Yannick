c2af1e36990228d609d5c71dfe5529173c586e3c
Nox57 <yannickparel@gmail.com>
2023-08-07 15:25:51 +0200

    Page 404 Not Found added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// Components
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+// Pages
 import Home from './pages/Home/Home'
+import NotFound from './pages/NotFound/NotFound'
+// Css
 import './App.css'
 
 function App() {
@@ -10,6 +14,7 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
             <Footer />
         </Router>
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,16 @@
+import './NotFound.css'
+
+function NotFound() {
+    return (
+        <main>
+            <div className="hero">
+                <section className="hero-content">
+                    <h2>Error 404</h2>
+                    <p>Whoops! The page you requested does not exist.</p>
+                </section>
+            </div>
+        </main>
+    )
+}
+
+export default NotFound
